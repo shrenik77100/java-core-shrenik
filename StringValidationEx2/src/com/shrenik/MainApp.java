65a4a80fedad03bab7924c00878a129de1593a70
shrenik77130 <shrenik77130@gmail.com>
2021-06-29 10:55:48 +0530

    Done

---
+++ StringValidationEx2/src/com/shrenik/MainApp.java
@@ -0,0 +1,22 @@
+package com.shrenik;
+
+import java.util.regex.*;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		String panNumber = "ABCDE134A";
+		String regExpr = "[A-Z]{5}\\d{4}[A-Z]";
+
+		
+		if(Pattern.matches(regExpr, panNumber))
+		{
+			System.out.println("Correct");
+		}
+		else
+		{
+			System.out.println("Incorrect");
+		}
+	}
+
+}
